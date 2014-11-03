2824dc75515b095ff68608528946645f9d99bfbe
igo903 <igo903@hotmail.com>
2014-11-03 14:34:00 +0800

    innove detail

--- innove_detail.php
+++ innove_detail.php
@@ -17,39 +17,44 @@
 			</div>
 
 			<div class="cont">
-				<div class="filter-panel">
+				<div class="direct-buy">
 					<div class="bread-nav">
-						<span>全部结果</span>
+						<span>首页</span>
 						<i>></i>
 						<span>创新产品</span>
 					</div>
-					<div class="content">
-						<h3>娱乐休闲多<span>商品筛选</span><span>(共<i>92</i>件商品)</span></h3>
-						<div class="row">
-							<span>已选条件:</span>
-							<a href="javascript:;" class="removeCondition"><i>按兴趣查询:</i><i class="ai">娱乐休闲多</i><i class="fa fa-close ai"></i></a>
-							<a href="javascript:;" class="removeSelected">全部撤销</a>
+					<div class="content fn-clear">
+						<div class="left-col">
+							<div class="snap-img">
+								<img src="images/mob_music.png" alt="">
+							</div>
+							<a class="saveto" href="javascript:;"><i class="fa fa-star"></i>收藏商品</a>
 						</div>
-						<div class="row">
-							<span>按拼音查询:</span>
-							<a href="javascript:;">C</a>
-							<a href="javascript:;">G</a>
-							<a href="javascript:;">H</a>
-							<a href="javascript:;">L</a>
-							<a href="javascript:;">Q</a>
-							<a href="javascript:;" class="selected">S</a>
-							<a href="javascript:;">W</a>
-							<a href="javascript:;">X</a>
-							<a href="javascript:;">Y</a>
-						</div>
-						<div class="row last">
-							<span>按类型查询:</span>
-							<a href="javascript:;">国际及港澳台漫游</a>
-							<a href="javascript:;">便民服务类</a>
-							<a href="javascript:;">娱乐媒体类</a>
-							<a href="javascript:;">商务生活类</a>
-							<a href="javascript:;">基础通信类</a>
-							<a href="javascript:;">3G特色类</a>
+						<div class="right-col">
+							<h3>手机电视</h3>
+							<h2>点播、下载、上传视频、还可以实时收看多个电视直播频道</h2>
+							<div class="price row">
+								<span class="de">资 费:</span>
+								<span class="priceNum">￥19</span><span>元/月</span>
+							</div>
+							<div class="row">
+								<span class="de">商品编号:</span>
+								<span>18970000</span>
+							</div>
+							<div class="row">
+								<span class="de">业务档次:</span>
+								<a href="javascript:;" class="selected">央视直播多频道包月</a>
+								<a href="javascript:;">东方龙直播多频道包月</a>
+								<a href="javascript:;">央广直播多频道包月</a>
+								<a href="javascript:;">无线江苏</a>
+							</div>
+							<div class="row">
+								<span class="de">生效方式:</span>
+								<a href="javascript:;" class="selected">立即生效</a>
+							</div>
+							<div class="row gobuy">
+								<a href="javascript:;">立即订购</a>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -60,6 +65,16 @@
 				<?php include("service_side.php"); ?>
 			</div><!--//service-side-->
 		</div><!--//home-part-1-->
+
+
+		<div class="detail-cont-wrap fn-clear">
+			<div class="left-col-hot">
+				
+			</div>
+			<div class="right-col-detail">
+				
+			</div>
+		</div>
 
 		
 
