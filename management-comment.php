64266661197731f87c4002a1c80578378e6e12e8
onetyb <onetyb@126.com>
2014-11-07 00:27:00 +0800

    onetyb  management2

--- management-comment.php
+++ management-comment.php
@@ -25,17 +25,166 @@
 							<div class="my-user">
 								<div class="my-img"><img src="images/meimage.jpg" /></div>
 			                    <div class="my-name">张三</div>
-			                    <div class="my-name my-name2">司徒斯托瑟</div>				                    					
+			                    <div class="my-name my-name2">司徒斯托瑟</div>
 							</div>
-							<div class="my-title"></div>
+							<div class="my-title">我的沃云</div>
 							<div class="my-link">
 								<ul>
 									<li>
-										<div class="myicon001"></div>
+										<div class="myicon01"></div>
              					    	<a href="#">订单中心</a>
              						</li>
+             						<li>
+										<div class="myicon02"></div>
+             					    	<a href="#">个人信息</a>
+             						</li>
+             						<li>
+										<div class="myicon03"></div>
+             					    	<a href="#">我的收藏</a>
+             						</li>
+             						<li>
+										<div class="myicon04"></div>
+             					    	<a href="#">收货地址管理</a>
+             						</li>
+             						<li class="myselected">
+										<div class="myicon06"></div>
+             					    	<a href="#">评价管理</a>
+             						</li>
+             						<li>
+										<div class="myicon05"></div>
+             					    	<a href="#">密码管理</a>
+             						</li>
+
 								</ul>
 							</div>	
+						</div>
+						<div class="right-col">
+							<div class="right-title">评价管理</div>
+							<div class="evaluate">
+								<h2>待评价商品：<b>1</b>个</h2>	
+								<div class="writing">
+									<div class="writing-title">
+				                        <div class="item-name">商品名称</div>
+				                        <div class="item-info">商品信息</div>
+				                        <div class="item-comm">评论</div>
+				                    </div>
+				                    <div class="writing-cont">
+				                        <div class="item-name">
+				                            <div class="picimg"><img src="images/evaluatepic.jpg" /></div>
+				                            <div class="picname">iPhone5s 金银灰色 指纹识别 送3600M流量</div>
+				                        </div>
+				                        <div class="item-info">
+				                            <p>颜色：金色</p>
+				                            <p>号码：18612345678（武汉）</p>
+				                            <p>套餐：286元3GiPhone套餐</p>
+				                            <p>合约：预存话费送手机</p>
+				                            <p>协议期：24个月</p>
+				                        </div>
+				                        <div class="item-comm fl"><span>评论</span></div>
+				                    </div>
+				                    <div class="writing-text">
+				                    	<div class="evaluatesj"></div>
+				                    	<div class="item">
+			                               	<span class="label"><em>*</em>评分：</span>
+			                                <div class="fl">
+			                                    <div class="wrapper">
+			                                        <a class="stara" ></a>
+			                                        <a class="stara" ></a>
+			                                        <a class="stara" ></a>
+			                                        <a class="stara" ></a>
+			                                        <a class="stara" ></a>
+			                                    </div>
+			                                </div>
+			                            </div>
+
+			                            <div class="item">
+				                             <span class="label"><em>*</em>标签：</span>
+				                             <div class="fl">
+				                                <ul class="tips-list">
+				                                    <li class="select"><s class="f-check"></s>效果不错</li>
+				                                    <li><s class="f-check"></s>物流速度快</li>
+				                                    <li><s class="f-check"></s>补水很好</li>
+				                                    <li><s class="f-check"></s>价格实惠</li>
+				                                    <li><s class="f-check"></s>比商场便宜</li>
+				                                    <li><s class="f-check"></s>还可以</li>
+				                                    <li><s class="f-check"></s>品牌好</li>
+				                                    <li><s class="f-check"></s>面膜不错</li>
+				                                    <li><s class="f-check"></s>脸上很舒服</li>
+				                                </ul>
+				                                
+				                                <div class="custom">
+				                                   <span>自定义：</span>
+				                                   <label for="textfield"></label>
+				                                   <input type="text" class="custominput" name="textfield2" id="textfield" /><a href="#">提交</a>
+				                                </div>
+				                             </div>
+				                        </div>
+
+				                        <div class="item">
+				                            <span class="label"><em>*</em>内容：</span>
+				                            <div class="item-cont">
+				                                <textarea name="" cols="" rows="" class="area area01">商品是否给力？快分享你的购买心得吧~</textarea>
+				                                <div class="clr"></div>
+				                                <div class="msg-text">10-500字</div>
+				                            </div>
+				                        </div>
+
+				                        <div class="item">
+				                            <span class="label"> </span>
+				                            <div class="fl">
+				                                <a href="#" class="btn">评价</a>
+				                            </div>
+				                        </div>
+				                    </div>
+								</div>
+
+								<div class="writing">
+									<div class="writing-title">
+				                        <div class="item-name">商品名称</div>
+				                        <div class="item-info">商品信息</div>
+				                        <div class="item-comm">评论</div>
+				                    </div>
+				                    <div class="writing-cont">
+				                        <div class="item-name">
+				                            <div class="picimg"><img src="images/evaluatepic.jpg" /></div>
+				                            <div class="picname">iPhone5s 金银灰色 指纹识别 送3600M流量</div>
+				                        </div>
+				                        <div class="item-info">
+				                            <p>颜色：金色</p>
+				                            <p>号码：18612345678（武汉）</p>
+				                            <p>套餐：286元3GiPhone套餐</p>
+				                            <p>合约：预存话费送手机</p>
+				                            <p>协议期：24个月</p>
+				                        </div>
+				                        <div class="item-comm fl"><span>评论</span></div>
+				                    </div>
+				                    <div class="writing-text">
+				                    	<div class="evaluatesj"></div>
+				                    	<div class="item">
+			                               	<span class="label"><em>*</em>评分：</span>
+			                                <div class="fl">
+			                                    <span class="commstar">
+			                                       	<div class="commstar2"></div>
+			                                    </span>
+			                                </div>
+			                            </div>
+
+			                            <div class="item">
+				                             <span class="label"><em>*</em>标签：</span>
+				                             <div class="fl">
+				                                <div class="item-text">大品牌   孩子喜欢   很方便</div>
+				                             </div>
+				                        </div>
+
+				                        <div class="item">
+				                            <span class="label"><em>*</em>内容：</span>
+				                            <div class="fl">
+				                               <div class="item-text">是国行正品，移动4G版，升级ios8.02有点卡，不过还是运行顺畅的。</div>
+				                            </div>
+				                        </div>
+				                    </div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
