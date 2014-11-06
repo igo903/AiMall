64e831b385d64aaa90a869b3cde7a5bc235703ca
onetyb <onetyb@126.com>
2014-11-06 18:06:19 +0800

    onetyb first management

---
+++ management-comment.php
@@ -0,0 +1,51 @@
+<?php include("header.php"); ?>
+
+
+			
+			<div id="main-nav" class="row">
+				<ul>
+					<li id="prds-pop"><a href="javascript:;" class="all-prds">所有商品分类</a></li>
+					<li><a href="index.php" >首页</a></li>
+					<li><a href="innovate.php" class="active">创新产品</a></li>
+					<li><a href="javascript:;">流量专区</a></li>
+					<li><a href="javascript:;">沃云.推荐</a></li>
+					<li><a href="javascript:;">团购</a></li>
+					<li><a href="javascript:;">限时购</a></li>
+				</ul>
+			</div>
+			<div class="cont">
+				<div class="management-panel">
+					<div class="bread-nav">
+						<span>首页</span>
+						<i>></i>
+						<span>创新产品</span>
+					</div>
+					<div class="content">
+						<div class="left-col">
+							<div class="my-user">
+								<div class="my-img"><img src="images/meimage.jpg" /></div>
+			                    <div class="my-name">张三</div>
+			                    <div class="my-name my-name2">司徒斯托瑟</div>				                    					
+							</div>
+							<div class="my-title"></div>
+							<div class="my-link">
+								<ul>
+									<li>
+										<div class="myicon001"></div>
+             					    	<a href="#">订单中心</a>
+             						</li>
+								</ul>
+							</div>	
+						</div>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="side service-side none" id="side-pop">
+				<?php include("service_side.php"); ?>
+			</div><!--//service-side-->
+		</div><!--//home-part-1-->
+
+
+<?php include("footer.php"); ?>
