ccd5925adf17ff5d6f3d67efabdb0c41997831d1
onetyb <onetyb@126.com>
2014-11-13 11:47:04 +0800

    floating layer2

--- tanchuang.php
+++ tanchuang.php
@@ -1,6 +1,7 @@
 <?php include("header.php"); ?>
-
-				<div id="dialog-inner" class="belonging overlay_content" style="display:block">
+<div style="background-color: #999; height:400px;">
+               <!-- 查看库存 -->
+				<!-- <div id="dialog-inner" class="belonging overlay_content" style="display:block">
 	                <div class="close_overlay"><a href="javascript:;"></a></div>
 	                <div class="white">
 	                	<div class="allcities">
@@ -8,14 +9,62 @@
 							<div class="dialog-content clearfix">
 			                    <select id="province" multiple="multiple" class="multiple-select"><option value="1" selected="">北京</option><option value="2">上海</option><option value="3">天津</option><option value="4">重庆</option><option value="5">河北</option><option value="6">山西</option><option value="7">河南</option><option value="8">辽宁</option><option value="9">吉林</option><option value="10">黑龙江</option><option value="11">内蒙古</option><option value="12">江苏</option><option value="13">山东</option><option value="14">安徽</option><option value="15">浙江</option><option value="16">福建</option><option value="17">湖北</option><option value="18">湖南</option><option value="19">广东</option><option value="20">广西</option><option value="21">江西</option><option value="22">四川</option><option value="23">海南</option><option value="24">贵州</option><option value="25">云南</option><option value="26">西藏</option><option value="27">陕西</option><option value="28">甘肃</option><option value="29">青海</option><option value="30">宁夏</option><option value="31">新疆</option><option value="32">台湾</option><option value="42">香港</option><option value="43">澳门</option><option value="84">钓鱼岛</option></select>
 			                    <select id="city" multiple="multiple" class="multiple-select"><option value="72">朝阳区</option><option value="2800">海淀区</option><option value="2801" selected="">西城区</option><option value="2802">东城区</option><option value="2803">崇文区</option><option value="2804">宣武区</option><option value="2805">丰台区</option><option value="2806">石景山区</option><option value="2807">门头沟</option><option value="2808">房山区</option><option value="2809">通州区</option><option value="2810">大兴区</option><option value="2812">顺义区</option><option value="2814">怀柔区</option><option value="2816">密云区</option><option value="2901">昌平区</option><option value="2953">平谷区</option><option value="3065">延庆县</option></select>
-			                    
+			                    <select id="city" multiple="multiple" class="multiple-select"><option value="72">朝阳区</option></select>
 			                </div>
 
 						</div>
 	                </div>
 	                
+				</div> -->
+
+
+				<!-- 删除商品 -->
+				<!-- <div id="switchndeleteContent" class="belonging overlay_content" style="display:block">
+	                <div class="close_overlay"><a href="javascript:;"></a></div>
+	                <div class="white">
+	                	<div class="allcities">
+							<h1>查看库存</h1>
+							<div class="deleteContent">
+								<p>确认从购物车删除该商品吗？</p>
+								<div class="switchnbuttn">
+									<a href="javascript:;" class="determine">确定</a>
+									<a href="javascript:;" class="cancel">取消</a>
+								</div>
+							</div>
+						</div>
+	                </div>	                
+				</div> -->
+
+				
+
+				<!-- 业务办理 -->
+				<div id="switchnbusinessContent" class="belonging overlay_content" style="display:block">
+	                <div class="close_overlay"><a href="javascript:;"></a></div>
+	                <div class="white">
+	                	<div class="allcities">
+							<h1>业务办理</h1>
+							<div class="business">
+								<p>请确认您要开通的业务信息</p>
+								<div>
+									<ul>
+										<li>
+											<span>客户号码</span>
+											<span>18512344321</span>
+										</li>
+										<li></li>
+										<li></li>
+										<li></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+	                </div>	                
 				</div>
 
+
+
+
+</div>
 
 				
 
