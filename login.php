d113115000c2415cfa09509c84dfa1745953a9b2
tianyb <onetyb@126.com>
2014-11-18 15:20:53 +0800

    reg

--- login.php
+++ login.php
@@ -14,27 +14,21 @@
 	<link rel="stylesheet" href="spm_modules/alice-button/1.3.0/button.css">
 	<link rel="stylesheet" href="spm_modules/fontawesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="spm_modules/alice-form/1.1.0/src/form.css">
-
 	<script type="text/javascript" src="spm_modules/seajs/2.3.0/sea-debug.js"></script>
 	<!--[if lt IE 9]>
 	    <script src="spm_modules/html5shiv/dist/html5shiv.js"></script>
 	<![endif]-->
 </head>
-
 <body>
-
-
-<div id="Ai_Wrap" class="wrap row">
-		
+<div id="Ai_Wrap" class="wrap row">		
 	<div class="screen-hd row" id="home-part-1">
-
 		
 		<div class="head">
 			<div class="logo">
 				<h1>
 					<a href="index.php" class="ailogo"></a>
 				</h1>
-				<b class="loginicon"></b>
+				<b class="loginicon">欢迎登录</b>
 			</div>
 		</div>
 		<div class="cont">
@@ -63,7 +57,9 @@
 							            <img class="ui-checkcode-imgcode-img" align="absMiddle" alt="请输入您看到的内容" src="https://omeo.alipay.com/service/checkcode?sessionID=82012ab6b1f4ed9e675fb9092a25cb3b&r=0.9321065918075809"  title="点击刷新图片校验码">
 							            <a href="#" class="imgcode-a">看不清，换一张</a>
 							        </div>
-							        <a href="#" class="btn loginbtn">登录</a>
+							        <div class="ui-form-item">
+							            <input type="submit" class="ui-button btn350" value="登录">
+							        </div>
 							    </fieldset>
 						  	</form>
 							<div class="entries">
