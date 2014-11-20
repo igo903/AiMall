f0b80a89139d98363c18ace6096d301941e0b7ac
tianyb <onetyb@126.com>
2014-11-20 15:21:24 +0800

    indent2

--- indent.php
+++ indent.php
@@ -79,6 +79,9 @@
 				                   <div class="column t-action">操作</div>
 				                </div>
 								<div class="shopping-prompt">最靓的顾客，由于您的商品属于不同商家活着不同仓库，所以您的订单会拆分为多个子订单配送，给您带来不便，敬请谅解！</div>
+								
+
+
 								<div class="orders-explode">
 				                    <b class="font_red">【已拆单】</b>
 				                    <span>订单号：730020356987671</span>
@@ -88,12 +91,16 @@
 				                <div class="orders-tbody">
 				                	<div class="item_selected"> 
 					                    <div class="goodbox">
-					                        <div class="goodbox-m">                   
+					                        <div class="goodbox-m"> 
 					                            <div class="cell p-goods">
-					                               	<div class="p-img"><a href="#" target="_blank"><img  src="images/phone10.jpg" alt=""></a></div>    
-					                                <div class="p-name">
-					                                <a href="#"  target="_blank" >iPhone5s 金银灰色 指纹识别 送3600M流量</a>
-					                                <p class="font_green">好友推荐最高得100元话费x1微信晒单得20元话费x1 </p></div>    
+					                               	<div class="p-img">
+					                               		<a href="#" target="_blank"><img  src="images/phone10.jpg" alt=""></a>
+					                               	</div>    
+					                                <div class="p-names">
+					                                	<a href="#"  target="_blank" >iPhone5s 金银灰色 指纹识别 送3600M流量</a>
+					                                	<p class="font_green">好友推荐最高得100元话费x1微信晒单得20元话费x1 </p>
+					                                	<p class="font_gray">商品编号：107891</p>
+					                                </div>    
 					                            </div>
 					                            <div class="cell p-message">
 					                                颜色：金色<br />
@@ -104,7 +111,68 @@
 					                            </div>
 					                            <div class="cell p-quantity">1</div>
 					                        </div>
-					                        <div class="goodbox-m">                   
+					                        <div class="goodbox-m">
+					                             <div class="cell p-goods">
+					                                <div class="p-img">
+					                               		<a href="#" target="_blank"><img  src="images/phone10.jpg" alt=""></a>
+					                               	</div>    
+					                                <div class="p-names">
+					                                	<a href="#"  target="_blank" >iPhone5s 金银灰色 指纹识别 送3600M流量</a>
+					                                	<p class="font_green">好友推荐最高得100元话费x1微信晒单得20元话费x1 </p>
+					                                	<p class="font_gray">商品编号：107891</p>
+					                                </div> 
+				                                </div>
+					                             <div class="cell p-message">
+					                                颜色：金色<br />
+					                                号码：18612345678（武汉）<br />
+					                                套餐：286元3GiPhone套餐<br />
+					                                合约：预存话费送手机<br />
+					                                协议期：24个月<br />
+					                             </div>
+					                             <div class="cell p-quantity">1</div>
+					                        </div>
+					                    </div>
+					                    <div class="cell p-people">张三<p>12345678914</p></div>
+					                    <div class="cell p-total">￥4000.00<p>网上支付</p></div>
+					                    <div class="cell p-inventory">待付款</div>
+					                    <div class="cell p-action">
+					                       <a href="#" class="payment">付款</a><br />
+					                       <a href="#">订单详情</a><br />
+					                       <a href="#" id="t-order">取消</a>
+					                    </div>				                	
+				                	</div>				                	
+				                </div>
+
+								<div class="orders-explode">
+				                    <b class="font_red">【已拆单】</b>
+				                    <span>订单号：730020356987671</span>
+				                    <span>下单时间：2014-07-11  12:00:01</span> 
+				                    <span class="send">由联通商家发货</span>
+				                </div>
+				                <div class="orders-tbody">
+				                	<div class="item_selected"> 
+					                    <div class="goodbox">
+					                        <div class="goodbox-m"> 
+					                            <div class="cell p-goods">
+					                               	<div class="p-img">
+					                               		<a href="#" target="_blank"><img  src="images/phone10.jpg" alt=""></a>
+					                               	</div>    
+					                                <div class="p-names">
+					                                	<a href="#"  target="_blank" >iPhone5s 金银灰色 指纹识别 送3600M流量</a>
+					                                	<p class="font_green">好友推荐最高得100元话费x1微信晒单得20元话费x1 </p>
+					                                	<p class="font_gray">商品编号：107891</p>
+					                                </div>    
+					                            </div>
+					                            <div class="cell p-message">
+					                                颜色：金色<br />
+					                                号码：18612345678（武汉）<br />
+					                                套餐：286元3GiPhone套餐<br />
+					                                合约：预存话费送手机<br />
+					                                协议期：24个月<br />
+					                            </div>
+					                            <div class="cell p-quantity">1</div>
+					                        </div>
+					                        <div class="goodbox-m">
 					                             <div class="cell p-goods">
 					                                <div class="p-img"><a href="#" target="_blank"><img  src="images/phone10.jpg" alt=""></a></div>    
 					                                <div class="p-name">
@@ -125,7 +193,91 @@
 					                    <div class="cell p-total">￥4000.00<p>网上支付</p></div>
 					                    <div class="cell p-inventory">待付款</div>
 					                    <div class="cell p-action">
-					                       <a href="#" class="payment">付款</a><br /><a href="#">订单详情</a><br /><a href="#" id="t-order">取消</a>
+					                       <a href="#" class="payment">付款</a><br />
+					                       <a href="#">订单详情</a><br />
+					                       <a href="#" id="t-order">取消</a>
+					                    </div>				                	
+				                	</div>				                	
+				                </div>
+
+				                <div class="orders-explode">
+				                    <b class="font_red">【已拆单】</b>
+				                    <span>订单号：730020356987671</span>
+				                    <span>下单时间：2014-07-11  12:00:01</span> 
+				                    <span class="send">由联通商家发货</span>
+				                </div>
+				                <div class="orders-tbody">
+				                	<div class="item_selected"> 
+					                    <div class="goodbox">
+					                        <div class="goodbox-m"> 
+					                            <div class="cell p-goods">
+					                               	<div class="p-img">
+					                               		<a href="#" target="_blank"><img  src="images/phone10.jpg" alt=""></a>
+					                               	</div>    
+					                                <div class="p-names">
+					                                	<a href="#"  target="_blank" >iPhone5s 金银灰色 指纹识别 送3600M流量</a>
+					                                	<p class="font_green">好友推荐最高得100元话费x1微信晒单得20元话费x1 </p>
+					                                	<p class="font_gray">商品编号：107891</p>
+					                                </div>    
+					                            </div>
+					                            <div class="cell p-message">
+					                                颜色：金色<br />
+					                                号码：18612345678（武汉）<br />
+					                                套餐：286元3GiPhone套餐<br />
+					                                合约：预存话费送手机<br />
+					                                协议期：24个月<br />
+					                            </div>
+					                            <div class="cell p-quantity">1</div>
+					                        </div>
+					                    </div>
+					                    <div class="cell p-people">张三<p>12345678914</p></div>
+					                    <div class="cell p-total">￥4000.00<p>网上支付</p></div>
+					                    <div class="cell p-inventory">待付款</div>
+					                    <div class="cell p-action">
+					                       <a href="#" class="payment">付款</a><br />
+					                       <a href="#">订单详情</a><br />
+					                       <a href="#" id="t-order">取消</a>
+					                    </div>				                	
+				                	</div>				                	
+				                </div>
+
+				                <div class="orders-explode">
+				                    <b class="font_red">【已拆单】</b>
+				                    <span>订单号：730020356987671</span>
+				                    <span>下单时间：2014-07-11  12:00:01</span> 
+				                    <span class="send">由联通商家发货</span>
+				                </div>
+				                <div class="orders-tbody">
+				                	<div class="item_selected"> 
+					                    <div class="goodbox">
+					                        <div class="goodbox-m"> 
+					                            <div class="cell p-goods">
+					                               	<div class="p-img">
+					                               		<a href="#" target="_blank"><img  src="images/phone10.jpg" alt=""></a>
+					                               	</div>    
+					                                <div class="p-names">
+					                                	<a href="#"  target="_blank" >iPhone5s 金银灰色 指纹识别 送3600M流量</a>
+					                                	<p class="font_green">好友推荐最高得100元话费x1微信晒单得20元话费x1 </p>
+					                                	<p class="font_gray">商品编号：107891</p>
+					                                </div>    
+					                            </div>
+					                            <div class="cell p-message">
+					                                颜色：金色<br />
+					                                号码：18612345678（武汉）<br />
+					                                套餐：286元3GiPhone套餐<br />
+					                                合约：预存话费送手机<br />
+					                                协议期：24个月<br />
+					                            </div>
+					                            <div class="cell p-quantity">1</div>
+					                        </div>
+					                    </div>
+					                    <div class="cell p-people">张三<p>12345678914</p></div>
+					                    <div class="cell p-total">￥4000.00<p>网上支付</p></div>
+					                    <div class="cell p-inventory">待付款</div>
+					                    <div class="cell p-action">
+					                       <div class="btn01" id="t2">修改入网号码</div>
+                       					   <div class="btn01" id="t-id">修改入网资料</div>
+					                       <a href="#">订单详情</a><br />
 					                    </div>				                	
 				                	</div>				                	
 				                </div>
