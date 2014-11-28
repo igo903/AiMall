ee7d636d10e4d0679d77579cf0c1a32ad9c7f75d
tianyb <onetyb@126.com>
2014-11-28 11:12:42 +0800

    announcement  and   announcementall

---
+++ announcement.php
@@ -0,0 +1,45 @@
+<?php include("header.php"); ?>
+
+			<div id="main-nav" class="row">
+				<ul>
+					<li id="prds-pop"><a href="javascript:;" class="all-prds">所有商品分类</a></li>
+					<li><a href="index.php" >首页</a></li>
+					<li><a href="innovate.php" class="active">创新产品</a></li>
+					<li><a href="javascript:;">流量专区</a></li>
+					<li><a href="javascript:;">沃云.推荐</a></li>
+					<li><a href="javascript:;">团购</a></li>
+					<li><a href="javascript:;">限时购</a></li>
+				</ul>
+			</div>
+
+			<div class="cont">	
+				<div class="noticebox">
+					<div class="noticeall">
+		              	<div class="noticecont">
+		                  	<div class="noticecont-title">10月14日至15日支撑系统升级，如造成不便敬请谅解！<p>2014-10-15  18:90:50</p></div>
+		                  	<div class="noticecont-cont">
+		                      	<div class="conta">尊敬的用户，您好：</div>
+		                      	<div class="contb">为更好的为您提供服务，亚信商城定于2014年10月14日17点至2014年10月15日9点对支撑系统进行升级，在此期间暂停4G用户的查询、缴费、业务办理等相关服务，给您带来的不便敬请谅解。为更好的为您提供服务，亚信商城定于2014年10月14日17点至2014年10月15日9点对支撑系统进行升级，在此期间暂停4G用户的查询、缴费、业务办理等相关服务，给您带来的不便敬请谅解。为更好的为您提供服务，亚信商城定于2014年10月14日17点至2014年10月15日9点对支撑系统进行升级，在此期间暂停4G用户的查询、缴费、业务办理等相关服务，给您带来的不便敬请谅解。为更好的为您提供服务，亚信商城定于2014年10月14日17点至2014年10月15日9点对支撑系统进行升级，在此期间暂停4G用户的查询、缴费、业务办理等相关服务，给您带来的不便敬请谅解。为更好的为您提供服务，亚信商城定于2014年10月14日17点至2014年10月15日9点对支撑系统进行升级，在此期间暂停4G用户的查询、缴费、业务办理等相关服务，给您带来的不便敬请谅解。为更好的为您提供服务，亚信商城定于2014年10月14日17点至2014年10月15日9点对支撑系统进行升级，在此期间暂停4G用户的查询、缴费、业务办理等相关服务，给您带来的不便敬请谅解。</div>
+		                      	<div class="contc">
+		                         	<p>客服团队</p>
+		                         	<p>2014年10月9日</p>
+		                      	</div>
+		                  	</div>
+		              	</div>
+		         	</div>
+
+				</div>
+
+
+			</div>
+
+			<div class="side service-side none" id="side-pop">
+					<?php include("service_side.php"); ?>
+			</div><!--//service-side-->
+		</div><!--//home-part-1-->
+
+	</div>
+
+
+<link rel="stylesheet" href="spm_modules/alice-paging/1.1.0/paging.css">
+<?php include("footer.php"); ?>
---
+++ announcementall.php
@@ -0,0 +1,101 @@
+<?php include("header.php"); ?>
+
+			<div id="main-nav" class="row">
+				<ul>
+					<li id="prds-pop"><a href="javascript:;" class="all-prds">所有商品分类</a></li>
+					<li><a href="index.php" >首页</a></li>
+					<li><a href="innovate.php" class="active">创新产品</a></li>
+					<li><a href="javascript:;">流量专区</a></li>
+					<li><a href="javascript:;">沃云.推荐</a></li>
+					<li><a href="javascript:;">团购</a></li>
+					<li><a href="javascript:;">限时购</a></li>
+				</ul>
+			</div>
+
+			<div class="cont">	
+				<div class="noticebox">
+			        <div class="noticeleft">
+		               	<div class="noticedd">公告分类</div>
+			               	<ul>
+		                        <li><a href="#">促销活动(34)</a></li>
+		                        <li class="active"><a href="#" >公告咨询(20)</a></li>
+			               	</ul> 
+			               	<div class="noticedd">最新公告</div>
+			               	<dd>
+			                  	<dt><a href="#">10月14日至15日支撑系10月14日至15日支撑系</a></dt>
+			                  	<dt><a href="#">10月14日至15日支撑系10月14日至15日支撑系</a></dt>
+			                  	<dt><a href="#">10月14日至15日支撑系10月14日至15日支撑系</a></dt>
+			                  	<dt><a href="#">10月14日至15日支撑系10月14日至15日支撑系</a></dt>
+			                  	<dt><a href="#">10月14日至15日支撑系10月14日至15日支撑系</a></dt>
+			                  	<dt><a href="#">10月14日至15日支撑系10月14日至15日支撑系</a></dt>
+			                </dd>
+			        </div>
+			         
+			        <div class="noticeright">
+		              	<div class="noticerighttitle">公告</div>
+		              	<div class="noticerightdd">
+			                <ul>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+				                <li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                   	<li>10月14日至15日支撑系统升级，如造成不便敬请谅解！<span>2014-10-15</span></li>
+			                </ul>
+			              </div>
+
+
+			              <div class="ui-paging row fn-right" id="pagination">
+						    <a href="#" class="ui-paging-prev">
+						        <i class="iconfont" title="左三角形">&#xF039;</i> 上一页
+						    </a>
+						    <a href="#" class="ui-paging-item">1</a>
+						    <a href="#" class="ui-paging-item ui-paging-current">2</a>
+						    <a href="#" class="ui-paging-item">3</a>
+						    <a href="#" class="ui-paging-item">4</a>
+						    <a href="#" class="ui-paging-item">5</a>
+						    <a href="#" class="ui-paging-item">6</a>
+						    <a href="#" class="ui-paging-item">7</a>
+						    <span class="ui-paging-ellipsis">...</span>
+						    <a href="#" class="ui-paging-item">24</a>
+						    <a href="#" class="ui-paging-next">
+						        下一页 <i class="iconfont" title="右三角形">&#xF03A;</i>
+						    </a>
+						    <span class="ui-paging-info"><span class="ui-paging-bold">5/7</span>页</span>
+						    <span class="ui-paging-which"><input name="some_name" value="6" type="text"></span>
+						    <a class="ui-paging-info ui-paging-goto" href="#">跳转</a>
+						</div>
+			              
+			              
+			        </div>
+			    </div>
+
+
+			</div>
+
+			<div class="side service-side none" id="side-pop">
+					<?php include("service_side.php"); ?>
+			</div><!--//service-side-->
+		</div><!--//home-part-1-->
+
+	</div>
+
+
+<link rel="stylesheet" href="spm_modules/alice-paging/1.1.0/paging.css">
+<?php include("footer.php"); ?>
