d699c5d43f8e7fd458a58a5699d01c6411915995
tianyb <onetyb@126.com>
2014-12-01 18:13:29 +0800

    collect
    新增我的收藏

---
+++ collect.php
@@ -0,0 +1,201 @@
+<?php include("header.php"); ?>
+
+
+			
+			<div id="main-nav" class="row">
+				<ul>
+					<li id="prds-pop"><a href="javascript:;" class="all-prds">所有商品分类</a></li>
+					<li><a href="index.php" >首页</a></li>
+					<li><a href="innovate.php" class="active">创新产品</a></li>
+					<li><a href="javascript:;">流量专区</a></li>
+					<li><a href="javascript:;">沃云.推荐</a></li>
+					<li><a href="javascript:;">团购</a></li>
+					<li><a href="javascript:;">限时购</a></li>
+				</ul>
+			</div>
+			<div class="cont">
+				<div class="management-panel">
+					<div class="bread-nav">
+						<span>首页</span>
+						<i>></i>
+						<span>创新产品</span>
+					</div>
+					<div class="content">
+						<?php include("personal_side.php"); ?>
+
+						<div class="right-col">
+							<div class="right-title">评价管理</div>
+							<div class="mycllect">
+								<div class="prd-list row">
+									<div class="channel-item">
+										<dl>
+											<dd class="p-img txtcenter">
+												<a href="javascript:;"><img src="images/mobile_xm.jpg" alt=""></a>
+											</dd>
+											<dt class="p-name">
+												<a href="javascript:;">小米 小米4 4G全国套餐合约机</a>
+											</dt>
+											<dd class="p-price">
+										    	<span class="g_price g_price-highlight"><span>¥</span><strong>1999.00</strong></span>
+										    </dd>
+										    <dd class="fn-btns">
+										    	<a href="javascript:;" class="btn buy">立即订购</a>
+										    	<a href="javascript:;" class="btn save">取消收藏</a>
+										    </dd>	
+									    </dl>
+									</div>
+									<div class="channel-item">
+										<dl>
+											<dd class="p-img txtcenter">
+												<a href="javascript:;"><img src="images/mobile_xlj.jpg" alt=""></a>
+											</dd>
+											<dt class="p-name">
+												<a href="javascript:;">小辣椒 LA5-W 默认开通炫铃升级版</a>
+											</dt>
+											<dd class="p-price">
+										    	<span class="g_price g_price-highlight"><span>¥</span><strong>1299.00</strong></span>
+										    </dd>
+										    <dd class="fn-btns">
+										    	<a href="javascript:;" class="btn buy">立即订购</a>
+										    	<a href="javascript:;" class="btn save">取消收藏</a>
+										    </dd>
+									    </dl>
+									</div>
+									<div class="channel-item">
+										<dl>
+											<dd class="p-img txtcenter">
+												<a href="javascript:;"><img src="images/mobile_s5.jpg" alt=""></a>
+											</dd>
+											<dt class="p-name">
+												<a href="javascript:;">三星S5（SM-G9006V）</a>
+											</dt>
+											<dd class="p-price">
+										    	<span class="g_price g_price-highlight"><span>¥</span><strong>4999.0</strong></span>
+										    </dd>
+										    <dd class="fn-btns">
+										    	<a href="javascript:;" class="btn buy">立即订购</a>
+										    	<a href="javascript:;" class="btn save">取消收藏</a>
+										    </dd>
+									    </dl>
+									</div>
+									<div class="channel-item  last">
+										<dl>
+											<dd class="p-img txtcenter">
+												<a href="javascript:;"><img src="images/mobile_s5.jpg" alt=""></a>
+											</dd>
+											<dt class="p-name">
+												<a href="javascript:;">三星S5（SM-G9006V）</a>
+											</dt>
+											<dd class="p-price">
+										    	<span class="g_price g_price-highlight"><span>¥</span><strong>4999.0</strong></span>
+										    </dd>
+										    <dd class="fn-btns">
+										    	<a href="javascript:;" class="btn buy">立即订购</a>
+										    	<a href="javascript:;" class="btn save">取消收藏</a>
+										    </dd>
+									    </dl>
+									</div>
+									<div class="channel-item">
+										<dl>
+											<dd class="p-img txtcenter">
+												<a href="javascript:;"><img src="images/mobile_hw.jpg" alt="华为（HUAWEI）MediaPad M1 (S8-303L) 4G/3G"></a>
+											</dd>
+											<dt class="p-name">
+												<a href="javascript:;">华为（HUAWEI）MediaPad M1 (S8-303L) 4G/3G</a>
+											</dt>
+											<dd class="p-price">
+										    	<span class="g_price g_price-highlight"><span>¥</span><strong>1777.0</strong></span>
+										    </dd>
+										    <dd class="fn-btns">
+										    	<a href="javascript:;" class="btn buy">立即订购</a>
+										    	<a href="javascript:;" class="btn save">取消收藏</a>
+										    </dd>
+									    </dl>
+									</div>
+									<div class="channel-item">
+										<dl>
+											<dd class="p-img txtcenter">
+												<a href="javascript:;"><img src="images/mobile_xm.jpg" alt=""></a>
+											</dd>
+											<dt class="p-name">
+												<a href="javascript:;">小米 小米4 4G全国套餐合约机</a>
+											</dt>
+											<dd class="p-price">
+										    	<span class="g_price g_price-highlight"><span>¥</span><strong>1999.00</strong></span>
+										    </dd>
+										    <dd class="fn-btns">
+										    	<a href="javascript:;" class="btn buy">立即订购</a>
+										    	<a href="javascript:;" class="btn save">取消收藏</a>
+										    </dd>	
+									    </dl>
+									</div>
+									<div class="channel-item">
+										<dl>
+											<dd class="p-img txtcenter">
+												<a href="javascript:;"><img src="images/mobile_xlj.jpg" alt=""></a>
+											</dd>
+											<dt class="p-name">
+												<a href="javascript:;">小辣椒 LA5-W 默认开通炫铃升级版</a>
+											</dt>
+											<dd class="p-price">
+										    	<span class="g_price g_price-highlight"><span>¥</span><strong>1299.00</strong></span>
+										    </dd>
+										    <dd class="fn-btns">
+										    	<a href="javascript:;" class="btn buy">立即订购</a>
+										    	<a href="javascript:;" class="btn save">取消收藏</a>
+										    </dd>
+									    </dl>
+									</div>
+									<div class="channel-item last">
+										<dl>
+											<dd class="p-img txtcenter">
+												<a href="javascript:;"><img src="images/mobile_s5.jpg" alt=""></a>
+											</dd>
+											<dt class="p-name">
+												<a href="javascript:;">三星S5（SM-G9006V）</a>
+											</dt>
+											<dd class="p-price">
+										    	<span class="g_price g_price-highlight"><span>¥</span><strong>4999.0</strong></span>
+										    </dd>
+										    <dd class="fn-btns">
+										    	<a href="javascript:;" class="btn buy">立即订购</a>
+										    	<a href="javascript:;" class="btn save">取消收藏</a>
+										    </dd>
+									    </dl>
+									</div>
+								</div>
+
+								<div class="ui-paging row fn-right" id="pagination">
+								    <a href="#" class="ui-paging-prev">
+								        <i class="iconfont" title="左三角形">&#xF039;</i> 上一页
+								    </a>
+								    <a href="#" class="ui-paging-item">1</a>
+								    <a href="#" class="ui-paging-item ui-paging-current">2</a>
+								    <a href="#" class="ui-paging-item">3</a>
+								    <a href="#" class="ui-paging-item">4</a>
+								    <a href="#" class="ui-paging-item">5</a>
+								    <a href="#" class="ui-paging-item">6</a>
+								    <a href="#" class="ui-paging-item">7</a>
+								    <span class="ui-paging-ellipsis">...</span>
+								    <a href="#" class="ui-paging-item">24</a>
+								    <a href="#" class="ui-paging-next">
+								        下一页 <i class="iconfont" title="右三角形">&#xF03A;</i>
+								    </a>
+								    <span class="ui-paging-info"><span class="ui-paging-bold">5/7</span>页</span>
+								    <span class="ui-paging-which"><input name="some_name" value="6" type="text"></span>
+								    <a class="ui-paging-info ui-paging-goto" href="#">跳转</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="side service-side none" id="side-pop">
+				<?php include("service_side.php"); ?>
+			</div><!--//service-side-->
+		</div><!--//home-part-1-->
+
+	</div>
+<link rel="stylesheet" href="spm_modules/alice-paging/1.1.0/paging.css">
+<?php include("footer.php"); ?>
