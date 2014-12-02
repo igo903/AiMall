a608949ff08c0248d6639a4c90e836602d8ee3cb
tianyb <onetyb@126.com>
2014-12-02 10:09:45 +0800

    u

---
+++ findPwd.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="ie ie6"><![endif]-->
+<!--[if IE 7]><html class="ie ie7"><![endif]-->
+<!--[if IE 8 ]><html class="ie8"><![endif]-->
+<!--[if !IE]><!-->
+<html lang="zh-CN">
+<!--<![endif]-->
+<head>
+	<meta charset="UTF-8">
+	<title>亚信商城</title>
+	<link rel="stylesheet" href="css/main.css">
+	<link rel="stylesheet" href="css/slick.css">
+	<link rel="stylesheet" href="spm_modules/alice-base/1.1.0/src/base.css">
+	<link rel="stylesheet" href="spm_modules/alice-button/1.3.0/button.css">
+	<link rel="stylesheet" href="spm_modules/fontawesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="spm_modules/alice-form/1.1.0/src/form.css">
+	<script type="text/javascript" src="spm_modules/seajs/2.3.0/sea-debug.js"></script>
+	<!--[if lt IE 9]>
+	    <script src="spm_modules/html5shiv/dist/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+<div id="topNav" class="site-nav">
+	<div class="mininavInner site-nav-bd">
+
+		<div class="fn-left">
+			<span>产业互联网-亚信商城开通啦！</span>
+			<a href="login.php>" class="ai">请登录</a>
+			<a href="javascript:;">免费注册</a>
+		</div>
+		
+		<div class="fn-right dropdowns">		
+			<div class="fn-left">
+				<a href="javascript:;" id="myMall">我的商城<i class="fa fa-angle-down" id="rm"></i></a>
+				<ul id="popupMyMall" class="fn-hide ui-popup">
+					<li><a href="javascript:;">我的订单</a></li>
+					<li><a href="javascript:;">个人信息</a></li>
+					<li><a href="javascript:;">收货地址维护</a></li>
+					<li><a href="javascript:;">我的收藏</a></li>
+				</ul>
+
+				<a href="javascript:;" class="" id="mobileMall">手机商城</a>
+				<a href="javascript:;" id="clientService">客户服务</a>
+				<a href="javascript:;" class="" id="siteMap">网站导航</a>
+				<a href="javascript:;" class="" id="bookmarkAi">收藏亚信商城</a>
+			</div>
+		</div>
+	</div>
+</div><!--//topnav-->
