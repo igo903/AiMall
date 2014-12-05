5c615996066a433f7bf83121a743825aa4c5f5b5
tianyb <onetyb@126.com>
2014-12-05 16:32:33 +0800

    help

--- help10.php
+++ help10.php
@@ -23,37 +23,7 @@
 					</div>
 
 					<div class="help-cont">
-						<div class="helpleft">
-				           	<div class="helplefttitle">帮助中心</div>
-				           	<div class="helpdd">购物指南</div>
-					           	<ul>
-				                    <li><a href="help1.php">购物流程</a></li>
-				                    <li><a href="help2.php" >发票说明</a></li>
-				                    <li><a href="help3.php">联系客服</a></li>
-				                    <li><a href="help4.php">注册协议</a></li>
-					           	</ul> 
-				           	<div class="helpdd">支付方式</div>
-					           	<ul>
-				                    <li><a href="help5.php">在线支付</a></li>
-					           	</ul> 
-				           	<div class="helpdd">配送方式</div>
-					           	<ul>
-				                    <li><a href="help6.php">快递配送</a></li>
-					           	</ul> 
-				           	<div class="helpdd">售后服务</div>
-					           	<ul>
-			                    	<li><a href="help7.php">退换货政策</a></li>
-				                    <li><a href="help8.php" >退换货流程</a></li>
-				                    <li><a href="help9.php">退款说明</a></li>
-				                    <li class="active"><a href="help10.php">手机维修</a></li>
-					           	</ul> 
-				           	<div class="helpdd">联通特色服务</div>
-					           	<ul>
-				                    <li><a href="help11.php">交费充值</a></li>
-				                    <li><a href="help12.php" >营业厅分布查询</a></li>
-				                    <li><a href="help13.php">号码归属地查询</a></li>
-					           	</ul> 
-		         		</div>
+						<?php include("help_side.php"); ?>
 
 		         		<div class="helpright">
 			              	<div class="helprighttitle">手机维修</div>
