c367e457f6f6fdd8ab8ece0f073f2846b27cfc61
tianyb <onetyb@126.com>
2014-12-09 12:48:19 +0800

    mycart3.php

---
+++ mycart3.php
@@ -0,0 +1,270 @@
+<?php include("header2.php"); ?>
+
+			<div id="main-nav" class="row">
+				<ul>
+					<li id="prds-pop"><a href="javascript:;" class="all-prds">所有商品分类</a></li>
+					<li><a href="index.php" >首页</a></li>
+					<li><a href="innovate.php" class="active">创新产品</a></li>
+					<li><a href="javascript:;">流量专区</a></li>
+					<li><a href="javascript:;">沃云.推荐</a></li>
+					<li><a href="javascript:;">团购</a></li>
+					<li><a href="javascript:;">限时购</a></li>
+				</ul>
+			</div>
+
+			<div class="cont">
+				<div class="mycartbox">
+					<div class="mycart-title">
+			            <div class="fl">我的购物车</div>
+			            <div class="fr">现在<a href="login.php">登录</a>您购物车中的商品将被永久保存</div>
+			        </div>
+			        <div class="shopping-prompt">订单中所有商品只能填写一份入网资料，如需填写多份，请分成多个订单提交</div>
+
+			        <div class="mycart-remind">
+			            <div class="s-time fl">请在<span class="font_red">29分37秒</span>内提交订单。下单后6小时内完成支付，否则您选择的号码资源会被释放。</div>
+			            <div class="cart-sum">
+			                已选商品<span class="font_red">2</span>件<b></b>合计：<span class="font_red">￥5059.00</span><a href="#">去结算</a>
+			            </div>
+			        </div>
+
+			        <div class="mycart-content">
+			        	<div class="cart-thead">
+		        			<div class="colum t-checkbox">
+		        				<input  type="checkbox" value="">
+                 			 	<label >全选</label>
+		        			</div>
+		        			<div class="column t-goods">商品</div>
+		        			<div class="column t-message">商品信息</div>
+               				<div class="column t-price">单价(元)</div>
+               				<div class="column t-inventory">
+		                       	<div id="inventory" >
+		                        	<span id="location" title="湖北省">湖北省</span>
+		                       	</div>
+               				</div>
+               				<div class="column t-quantity">数量</div>
+               				<div class="column t-total">小计</div>
+               				<div class="column t-action">操作</div>
+		        		</div>
+			        	<div class="cart-tbody"> 
+		        			<div class="item_selected">
+		        				<div class="cell p-checkbox">
+			                        <input class="checkbox" type="checkbox" name="checkItem" checked=""  >
+			                    </div>
+
+			                    <div class="cell p-goods">
+			        				<div class="p-img">
+			        					<a href="#" target="_blank"><img  src="images/phone10.jpg" alt=""></a>
+			        				</div>    
+				                    <div class="p-names">
+				                        <a href="#"  target="_blank" >iPhone5s 金银灰色 指纹识别 送3600M流量</a>
+				                        <p class="font_red">好友推荐最高得100元话费x1微信晒单得20元话费x1 </p>
+				                    </div>
+			        			</div>
+
+			        			<div class="cell p-message">
+			                        颜色：金色<br />
+			                        号码：18612345678（武汉）<br />
+			                        套餐：286元3GiPhone套餐<br />
+			                        合约：预存话费送手机<br />
+			                        协议期：24个月<br />
+			                    </div>
+
+			                    <div class="cell p-price">￥4999.00<p>￥5200.00</p></div>
+			                    <div class="cell p-inventory">有货</div>
+			                    <div class="cell p-quantity">
+				                     <div class="quantity-form ">
+				                        <a class="reduce" href="javascript:;">-</a>
+				                        <input type="text" name="qty_item_1" value="1" id="qty_item_1" class="count-text" />
+				                        <a class="add" href="javascript:;">+</a>
+				                     </div>
+			                    </div>
+ 								<div class="cell p-total">
+			                       ￥4999.00
+			                       <p>(含号码预存款￥：9999.00)</p>
+			                    </div>
+			                    <div class="cell p-action">
+			                       <a href="#" class="p-del">删除</a><br /><a href="#" id="t1">收藏</a>                    
+			                    </div>
+		        			</div>
+
+		        			<div class="item_selected">
+		        				<div class="cell p-checkbox">
+			                        <input class="checkbox" type="checkbox" name="checkItem" checked=""  >
+			                    </div>
+
+			                    <div class="cell p-goods">
+			        				<div class="p-img">
+			        					<a href="#" target="_blank"><img  src="images/phone10.jpg" alt=""></a>
+			        				</div>    
+				                    <div class="p-names">
+				                        <a href="#"  target="_blank" >iPhone5s 金银灰色 指纹识别 送3600M流量</a>
+				                        <p class="font_red">好友推荐最高得100元话费x1微信晒单得20元话费x1 </p>
+				                    </div>
+			        			</div>
+
+			        			<div class="cell p-message">
+			                        颜色：金色<br />
+			                        号码：18612345678（武汉）<br />
+			                        套餐：286元3GiPhone套餐<br />
+			                        合约：预存话费送手机<br />
+			                        协议期：24个月<br />
+			                    </div>
+
+			                    <div class="cell p-price">￥4999.00<p>￥5200.00</p></div>
+			                    <div class="cell p-inventory"><span>库存紧张</span></div>
+			                    <div class="cell p-quantity">
+				                     <div class="quantity-form ">
+				                        <a class="reduce" href="javascript:;">-</a>
+				                        <input type="text" name="qty_item_1" value="1" id="qty_item_1" class="count-text" />
+				                        <a class="add" href="javascript:;">+</a>
+				                     </div>
+			                    </div>
+ 								<div class="cell p-total">
+			                       ￥4999.00
+			                      
+			                    </div>
+			                    <div class="cell p-action">
+			                       <a href="#" class="p-del">删除</a><br /><a href="#" id="t1">收藏</a>                    
+			                    </div>
+		        			</div>
+
+		        			<div class="cart-gifts">
+		        				<div class="giftsicon"></div>
+		        				<div class="gifts-item">
+		        					<span class="gifts-name">[赠品] 华为 原装线控带麦耳塞式耳机 （白色）</span>
+		        					<span>×1</span>
+		        				</div>
+		        				<div class="gifts-item">
+		        					<span class="gifts-name">[赠品] 华为 原装屏幕保护膜 （高透） 适用于荣耀 3C</span>
+		        					<span>×1</span>
+		        				</div>
+		        				<div class="gifts-item">
+		        					<span class="gifts-name">[赠品] 华为 原装保护壳 （半透明） 适用于荣耀 3C</span>
+		        					<span>×1</span>
+		        				</div>
+		        			</div>
+		        			
+		        			<div class="item_selected item_oos">
+		        				<div class="cell p-checkbox">
+			                        <input class="checkbox" type="checkbox" name="checkItem"  disabled="disabled">
+			                    </div>
+
+			                    <div class="cell p-goods">
+			        				<div class="p-img">
+			        					<a href="#" target="_blank"><img  src="images/phone10.jpg" alt=""></a>
+			        				</div>    
+				                    <div class="p-names">
+				                        <a href="#"  target="_blank" >iPhone5s 金银灰色 指纹识别 送3600M流量</a>
+				                        <p class="font_reg">该商品已下架或无库存 </p>
+				                    </div>
+			        			</div>
+
+			        			<div class="cell p-message">
+			                        颜色：金色<br />
+			                        号码：18612345678（武汉）<br />
+			                        套餐：286元3GiPhone套餐<br />
+			                        合约：预存话费送手机<br />
+			                        协议期：24个月<br />
+			                    </div>
+
+			                    <div class="cell p-price">￥4999.00<p>￥5200.00</p></div>
+			                    <div class="cell p-inventory">无货</div>
+			                    <div class="cell p-quantity">
+				                     <div class="quantity-form ">
+				                        <a class="reduce" href="javascript:;">-</a>
+				                        <input type="text" name="qty_item_1" value="1" id="qty_item_1" class="count-text" />
+				                        <a class="add" href="javascript:;">+</a>
+				                     </div>
+			                    </div>
+ 								<div class="cell p-total">
+			                       ￥4999.00
+			                    </div>
+			                    <div class="cell p-action">
+			                       <a href="#" class="p-del">删除</a><br /><a href="#" id="t1">收藏</a>                    
+			                    </div>
+							</div>
+			        			
+
+							<div class="cart-total">
+				                <div class="delete form">
+			                        <input  id="toggle-checkboxes" type="checkbox" value="">
+			                      	<label for="toggle-checkboxes">全选</label>
+			                      	<a href="#" >删除</a>                  
+				                </div>
+				                <div class="total">已选商品<span>2</span>件<b></b>合计：<span >¥2320.00</span></div>
+				            </div>
+			        	</div>						
+			        </div>
+
+			        <div class="cart-btn">
+				            <a href="#" class="goonbuy">继续购物</a>
+				            <a href="orders.php" class="goaccounps">去结算</a>
+				    </div>
+				</div>
+
+
+				<div class="recommend-panel fn-clear mt12">
+		    		<div class="col-main">
+						
+							<div class="hotsell hoticon">
+								<h3>热卖</h3>
+								<ul>		
+									<li>
+										<img src="images/601_90x90.jpg" alt="">
+										<div class="info">
+											<a href="javascript:;">iPhone5S 默认开通炫铃升级版 </a>
+											<i>￥4899.00</i>
+											<a href="javascript:;" class="btn buy">立即订购</a>
+										</div>
+									</li>
+									<li>
+										<img src="images/659_90x90.jpg" alt="">
+										<div class="info">
+											<a href="javascript:;">iPhone5S 默认开通炫铃升级版 </a>
+											<i>￥4899.00</i>
+											<a href="javascript:;" class="btn buy">立即订购</a>
+										</div>
+									</li>
+									<li>
+										<img src="images/546_90x90.jpg" alt="">
+										<div class="info">
+											<a href="javascript:;">iPhone5S 默认开通炫铃升级版 </a>
+											<i>￥4899.00</i>
+											<a href="javascript:;" class="btn buy">立即订购</a>
+										</div>
+									</li>
+									<li>
+										<img src="images/611_90x90.jpg" alt="">
+										<div class="info">
+											<a href="javascript:;">iPhone5S 默认开通炫铃升级版 </a>
+											<i>￥4899.00</i>
+											<a href="javascript:;" class="btn buy">立即订购</a>
+										</div>
+									</li>
+									<li>
+										<img src="images/659_90x90.jpg" alt="">
+										<div class="info">
+											<a href="javascript:;">iPhone5S 默认开通炫铃升级版 </a>
+											<i>￥4899.00</i>
+											<a href="javascript:;" class="btn buy">立即订购</a>
+										</div>
+									</li>
+								</ul>
+							</div>
+						
+					</div>
+		    	</div>
+			</div><!--//cont-->
+
+
+
+
+			<div class="side service-side none" id="side-pop">
+					<?php include("service_side.php"); ?>
+				</div><!--//service-side-->
+			</div><!--//home-part-1-->
+
+	</div>
+
+<link rel="stylesheet" href="spm_modules/alice-step/1.1.0/index.css">
+<?php include("footer.php"); ?>
